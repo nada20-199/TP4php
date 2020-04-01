94cb1c427ad592059641ed4f90cfd770a62af6c4
nada20-199 <62386465+nada20-199@users.noreply.github.com>
2020-04-01 21:01:15 +0200

    Add files via upload

---
+++ TP4 php/TP4EXO2.php
@@ -0,0 +1,89 @@
+<html>
+
+<head>
+</head>
+
+<body>
+    <h2>Commande clients</h2>
+
+    <?php 
+    $tab= file("pscde01_CLI1001.txt");
+    //echo count($tab);
+    for($i = 0; $i<count($tab);$i++)
+    {
+      $a = explode("|",$tab[$i]);
+        print_r ($a);
+    }
+    
+    ?>
+
+    <table border="red 2px solid">
+        <th>Numéro de commmandes</th>
+        <th>Numéro Clients</th>
+        <th>Date de commandes</th>
+        <th>Designation Article</th>
+        <th>Quantité(PAL)</th>
+        <th>Prix unitaires(HT)</th>
+		<th>Date de Livraison</th>
+		<th>Adresse Client</th>
+    <?php 
+    $tab= file("pscde01_CLI1001.txt");
+    //echo count($tab);
+    for($i = 0; $i<count($tab);$i++)
+    { echo "<tr>";
+      $a = explode("|",$tab[$i]);
+        print_r ($a);
+     for($j=0;$j<count($a);$j++)
+     {
+         echo "<td>";
+         echo $a[$j];
+         echo "</td>";
+     }
+     echo "</tr>";
+    }
+    
+    ?>
+    </table>
+	 <?php 
+    $tab= file("psccl01_CLI1004.txt");
+    //echo count($tab);
+    for($i = 0; $i<count($tab);$i++)
+    {
+      $a = explode("|",$tab[$i]);
+        print_r ($a);
+    }
+    
+    ?>
+
+	  <table border="red 2px solid">
+        <th>Numéro de commmandes</th>
+        <th>Numéro Clients</th>
+        <th>Date de commandes</th>
+        <th>Designation Article</th>
+        <th>Quantité(PAL)</th>
+        <th>Prix unitaires(HT)</th>
+		<th>Date de Livraison</th>
+		<th>Adresse Client</th>
+    <?php 
+    $tab= file("psccl01_CLI1004.txt");
+    //echo count($tab);
+    for($i = 0; $i<count($tab);$i++)
+    { echo "<tr>";
+      $a = explode("|",$tab[$i]);
+        print_r ($a);
+     for($j=0;$j<count($a);$j++)
+     {
+         echo "<td>";
+         echo $a[$j];
+         echo "</td>";
+     }
+     echo "</tr>";
+    }
+    
+    ?>
+    </table>
+    
+
+</body>
+
+</html>
---
+++ TP4 php/pageIndex.php
@@ -0,0 +1,13 @@
+<?php
+session_start(); 
+
+    $password=$_POST['pass'];
+ 
+    if (preg_match('/^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.\W).{8,}$/', $password)) {
+        echo 'Mot de passe conforme';
+	}
+	
+    else {
+        echo 'Mot de passe non conforme';
+	}	
+?>
